1862b367712353886170ff3cfc8eb4cbf867cfde
jakelester66 <jl6650@bard.edu>
2020-01-07 15:57:23 -0500

    some changes

--- twitter/Senators.py
+++ twitter/Senators.py
@@ -2,27 +2,29 @@
 import sys
 import os
 import pickle
-import datetime
+from datetime import datetime
+import tweepy
+import twitter_credentials
 
-sys.path.append(os.getcwd() + "\\parameters")
-dirpath = os.getcwd().split("\\")
+# set up authorization
+auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
 
-
+# make API #TODO put this guy in the place it needs to be and then just pass auth as an arg
+API = tweepy.API(auth)
 
 
 class Senators():
     """
-    Deals with the 2step transformation of
-        senators.txt
-        -> ndarray(100,4) [state, firstname, lastname, handle]
-        -> ndarray(100,4) [state, firstname, lastname, handle, userID]
+    class to help with transformin raw senator data into list of twitter user-ids
+    also does other things, like make list of user objects
     """
 
-    def __init__(self, senators_filename=False, run_all=False, api=False):
+    def __init__(self, senators_filename="senators.txt", run_all=False, api=False):
         self.senator_screen_names = []
         self.senator_user_ids = []
         self.senator_objects = []
-        self.api=api
+        self.api = api
         if run_all:
             assert senators_filename
             self.run_all(senators_filename)
@@ -59,8 +61,9 @@ class Senators():
         d = self.load_data(senators_filename)
         self.make_senator_list(d)
         self.make_senator_user_object_list()
+        self.make_senator_user_id_list()
 
-    def save_data(self, data, fpn):
+    def save_data_pickle(self, data, fpn):
 
         assert (fpn[-4:] == ".dat"), fpn
         with open(fpn, "wb") as f:
@@ -75,22 +78,22 @@ class Senators():
     def update_senator_user_id_list(self, user):
         self.senator_user_ids.append(user.id_str)
 
-    def make_senator_user_id_list(self, user_object_list):
-        for user in user_object_list:
+    def make_senator_user_id_list(self):
+        for user in self.senator_objects:
             self.update_senator_user_id_list(user)
 
 
 if __name__ == "__main__":
     # note: must be run from twitter\\ dir
 
-    s_fn = os.getcwd() + "\\senators\\senators.txt"
-    s = Senators(senators_filename=s_fn, run_all=True)
-    print(s.senator_objects[0])
+    # s_fn = os.getcwd() + "\\senators\\senators.txt"
+    s = Senators(run_all=True, api=API)
+    print(s.senator_user_ids)
 
-    today = str(datetime.datetime.today().strftime('%d-%m-%Y'))
-    users_fpn = os.getcwd() + "\\output\\senator_users_" + today + ".dat"
+    # today = str(datetime.datetime.today().strftime('%d-%m-%Y'))
+    # users_fpn = os.getcwd() + "\\output\\senator_users_" + today + ".dat"
 
-    s.save_data(s.senator_objects, users_fpn)
+    # s.save_data(s.senator_objects, users_fpn)
 
-    loaded_senators = s.load_data_pickle(users_fpn)
-    print(loaded_senators[0])
+    # loaded_senators = s.load_data_pickle(users_fpn)
+    # print(loaded_senators[0])
---
+++ twitter/tweepy_streamer.py
@@ -0,0 +1,98 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy import API
+
+import twitter_credentials
+import tweepy_filter_parameters
+import datetime
+import time
+
+
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets
+    """
+
+
+    def stream_tweets(self, auth, filter_params, save_file, time_limit=False):
+        # This handles Twitter authentication and the connection to the Twitter Streaming API
+        # Input:
+        # auth: twitter authorization
+        # filter_params : dictionary containing words to track and accounts to follow
+        # save_file : location to save tweets
+        # stop_time : market hours default, otherwise stops after this many seconds
+
+        track_list = filter_params["track_list"]
+        follow_list = filter_params["follow_list"]
+        async_bool = filter_params["async_bool"]
+
+        ## moved to main
+        # auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        # auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+
+        listener = MyStreamListener(save_file, time_limit)
+
+        stream = Stream(auth, listener)
+
+        # filter stream
+        stream.filter(follow=follow_list, track=track_list, is_async=async_bool)
+
+
+class MyStreamListener(StreamListener):  # inherits from StreamListener
+    """
+    listener class that writes fetched tweets to file until market close
+    """
+
+    def __init__(self, save_file, time_limit):
+        self.save_file = open(save_file, 'a')
+        self.time_limit = time_limit
+        self.start_time = time.time()
+
+    def from_original(self, status):
+        # ensures our streamer only gets tweets from our folow list, not responses to
+        # taken from @nelvintan tweepy issues 981
+        if hasattr(status, 'retweeted_status'):
+            return False
+        elif status.in_reply_to_status_id != None:
+            return False
+        elif status.in_reply_to_screen_name != None:
+            return False
+        elif status.in_reply_to_user_id != None:
+            return False
+        else:
+            return True
+
+    def on_data(self, data):
+        # writes data to save_file if market hours or by time limit
+        if not self.time_limit:
+            print("broken")
+            if datetime.now().hour >= 16:  # market is closed at 16:00
+                self.save_file.close()
+                return False
+            else:
+                try:
+                    self.save_file.write(data)
+                    return True
+                except BaseException as e:
+                    print("Error on_data: %s" % str(e))
+            return True
+        else:
+            if self.time_limit <= time.time() - self.start_time:
+                self.save_file.close()
+                return False
+            else:
+                try:
+                    self.save_file.write(data)
+
+                    return True
+                except BaseException as e:
+                    print("Error on_data: %s" % str(e))
+            return True
+
+
+    def on_error(self, status):
+        if status == 420:
+            # returning False in on_data disconnects the stream
+            return False
+
