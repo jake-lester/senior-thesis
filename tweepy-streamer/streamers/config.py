c880bd384fbb8f175f2472a4c2828a105875c7ac
Jake Lester <jl6650@bard.edu>
2020-02-26 13:44:28 -0400

    print

--- tweepy-streamer/streamers/config.py
+++ tweepy-streamer/streamers/config.py
@@ -13,6 +13,11 @@ def create_api():
     access_token = os.getenv("ACCESS_TOKEN")
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
+    print('consumer_key', consumer_key)
+    print("consumer_secret", consumer_secret)
+    print("access_token", access_token)
+    print("access_token_secret", access_token_secret)
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
