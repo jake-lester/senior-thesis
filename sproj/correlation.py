91d8fdb5ee749b426e70f5bf772ca9d481a04221
Jake Lester <jl6650@bard.edu>
2020-05-06 10:53:12 -0400

    corelation

--- sproj/correlation.py
+++ sproj/correlation.py
@@ -1,4 +1,4 @@
-## corelation.py
+## correlation.py
 import common.dates as dates
 import common.misc as misc
 import pandas as pd
@@ -11,35 +11,36 @@ from load import prepare_data
 
 
 class CorMatrix:
+    """
+    Object used for generating corelations between sentiment and financial data across different date groupings and
+    varying temporal shifts.
+    """
 
-    def __init__(self, vix_df, spx_df, tweet_df, group=None, delta=None):
+    def __init__(self, vix_df, spx_df, tweet_df):
         self.vix_df = vix_df
         self.spx_df = spx_df
         self.tweet_df = tweet_df
-        #self.group = group
-        #self.delta = delta  # optional
-        #self.data = self.prepare_data()
-
-    def delete_dataframes(self):
-        self.vix_df, self.spx_df, self.tweet_df = None, None, None
+        self.data = None
 
     def prepare_data(self):
-        # tweet_df=remove_neutral_sentiment(tweet_df)
-        ##self.group_data()
-        self.vix_df = self.vix_df.pct_change()  # or pct change?
+        """
+        Calculate and update financial data with percent change. Then concatonate all data into one DataFrame
+        :return: DataFrame
+        """
+        self.vix_df = self.vix_df.pct_change()
         self.spx_df = self.spx_df.pct_change()
         return self.concat_data()
 
+    def concat_data(self):
+        return pd.concat([self.vix_df, self.spx_df, self.tweet_df], axis=1, sort=True)
+
     def group_data(self, group):
-        # Group data by time period
+        """ Group data by time period """
         self.vix_df = dates.group_data(self.vix_df, ['vix_close'], method='lastValue', group=group)
         self.vix_df.drop(labels='datetime', axis=1, inplace=True)
         self.spx_df = dates.group_data(self.spx_df, ['spx_close'], method='lastValue', group=group)
         self.spx_df.drop(labels='datetime', axis=1, inplace=True)
         self.tweet_df = dates.group_data(self.tweet_df, ['nltk', 'flair'], method='avg', group=group)
-
-    def concat_data(self):
-        return pd.concat([self.vix_df, self.spx_df, self.tweet_df], axis=1, sort=True)
 
     def shift_vix_by_delta(self, delta):
         self.vix_df.datetime = self.vix_df['datetime'] - timedelta(minutes=delta)
@@ -56,18 +57,14 @@ class CorMatrix:
     def get_cor_matrix(self, method='pearson'):
         return self.data.corr(method=method)
 
-    def plot_score_vs_spx(self, x='spx', y='vix'):
-        self.data.plot(x=x, y=y, kind='scatter')
-        plt.show()
-
-def plot_all_cor(data):
-    data.unstack(level=0)
-    print(data)
-    d1 = data["spx:vix"].unstack(level=1)
-    xvals = d1.index
-
 
 def make_data(groups, deltas):
+    """
+    Creates empty MultiIndex DataFrame to store correlation values
+    :param groups: List of Strings
+    :param deltas: List of Integers
+    :return: MultiIndex DataFrame
+    """
     tuples = misc.combinations(groups, deltas)
     index = pd.MultiIndex.from_tuples(tuples, names=['groups', 'deltas'])
     data = pd.DataFrame(data=0, index=index, columns=['spx:nltk',
@@ -75,17 +72,18 @@ def make_data(groups, deltas):
                                                       'vix:nltk',
                                                       'vix:flair',
                                                       'spx:vix',
-                                                      'nltk:flair'],
-                        dtype=float)
-
+                                                      'nltk:flair'], dtype=float)
     return data
 
 
 def iterate_cor(cormatrix, data, method):
     """
-    DELTA MEASURED IN MINUTES
+    Generates DataFrame of correlations across different combinations of group by and shift by values.
+    NOTE! Correlations between nltk:flair and vix:spx are always with a delta of zero
     :param cormatrix: CorMatrix Object
-    :return:
+    :param data: MultiIndex DataFrame
+    :param method: String in {'pearson', 'kendal', 'spearman'}
+    :return: MultiIndex DataFrame
     """
     import copy
     for x in data.index:
@@ -93,7 +91,6 @@ def iterate_cor(cormatrix, data, method):
         group, delta = x[0], x[1]
         cormatrix_copy.shift_spx_by_delta(delta)
         cormatrix_copy.shift_vix_by_delta(delta)
-        #cormatrix_copy.shift_tweet_by_delta(delta)
         cormatrix_copy.group_data(group)
         cormatrix_copy.data = cormatrix_copy.prepare_data()
         matrix = cormatrix_copy.get_cor_matrix(method=method)
@@ -106,11 +103,16 @@ def iterate_cor(cormatrix, data, method):
 
     return data
 
-def create_corelation_data(groups, deltas):
+
+def create_correlation_data(groups, deltas):
     """
-    neut_sentiment -- "include_neut" or "exclude_neut"
+    Fills MultiIndex DataFrames with correlations according to method.
+    :param groups: List of Strings where last char is 'T' and preceding chars are Ints.
+    :param deltas: List of Integers to shift Financial data by... measured in minutes
+    :return: Nested Dictionary with correlation method as key and MultiIndex DataFrames as values. The parent key is
+    whether or not neutral sentiment was included.
     """
-    DATA = {
+    all_data = {
         'include_neut': {
             'pearson': None,
             'kendall': None,
@@ -125,25 +127,26 @@ def create_corelation_data(groups, deltas):
     co = CorMatrix(vix_df, spx_df, tweet_df)
     for method in ['pearson', 'kendall', 'spearman']:
         data = make_data(groups, deltas)
-        DATA['include_neut'][method] = iterate_cor(co, data, method)
+        all_data['include_neut'][method] = iterate_cor(co, data, method)
 
     vix_df, spx_df, tweet_df = prepare_data(remove_neutral_sent=True)
     co = CorMatrix(vix_df, spx_df, tweet_df)
     for method in ['pearson', 'kendall', 'spearman']:
         data = make_data(groups, deltas)
-        DATA['exclude_neut'][method] = iterate_cor(co, data, method)
+        all_data['exclude_neut'][method] = iterate_cor(co, data, method)
 
-    return DATA
+    return all_data
+
 
 if __name__ == "__main__":
-    vix_df, spx_df, tweet_df = prepare_data()
-    co = CorMatrix(vix_df, spx_df, tweet_df)
-    groups = ["1T", "5T", "15T", "30T", "60T", "120T", "180T"]#[str(i) + 'T' for i in range(1, 4)]
-    deltas = [i for i in range(0, 8)]
-    DATA = make_data(groups, deltas)
-    DATA = iterate_cor(co, DATA)
-    #plot_all_cor(DATA)
-    #update.add_corelations(DATA)
+    VIX_DF, SPX_DF, TWEET_DF = prepare_data()
+    CO = CorMatrix(VIX_DF, SPX_DF, TWEET_DF)
+    GROUPS = ["1T", "5T", "15T", "30T", "60T", "120T", "180T"]
+    DELTAS = [i for i in range(0, 8)]
+    DATA = make_data(GROUPS, DELTAS)
+    DATA = iterate_cor(CO, DATA)
+    # plot_all_cor(DATA)
+    # update.add_correlations(DATA)
 
     ''' Commented out to debug
 if __name__ == "__main__":
@@ -164,9 +167,8 @@ if __name__ == "__main__":
     #print(max(spx_score_cor.items()))
     '''
 
-
     """
     why are there so many nan values for cor?
     Maybe we should standardize dates?
-    perhaps we remove nan rows before corelation
+    perhaps we remove nan rows before correlation
     """
