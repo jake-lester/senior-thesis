474b355e36e74f4ff8527a82d3780781f02aabb8
Jake Lester <jl6650@bard.edu>
2020-05-14 21:44:27 -0400

    documentation

--- sproj/common/dates.py
+++ sproj/common/dates.py
@@ -2,21 +2,17 @@
 
 import pandas as pd
 
-def group_data(data, value_col_names, method="lastValue", group='D'):
-    """
-    Creates new dates, y arrays that go by group interval
-    Keyword arguments:
-        data -- dataframe with a dates column and specified value column
-        value_col_names -- list of string name of columns that will be used as y value data
-        method -- string to decide how to determine y value
-            "lastValue" -- take the last y value of a day
-            "avg" -- average the y values in a day
-        group -- DataOffset, Timedelta, or str. What time period to group by
-    :return new dataframe
-    """
 
+def group_data(data, value_col_names, method="lastValue", group='1440T'):
+    """
+    Group data by specified method and Group value
+    :param data: DataFrame with DateTime index
+    :param value_col_names: List of Strings
+    :param method: String in {'lastValue', 'avg', 'count'}
+    :param group: String where last character is 'T' and preceding chars are Ints
+    :return: DataFrame with Index and specified value_col_names grouped by group and method
+    """
     df = pd.DataFrame()
-
     df['datetime'] = pd.to_datetime(data['datetime'])
     for value_col_name in value_col_names:
         df[value_col_name] = pd.to_numeric(data[value_col_name])
@@ -29,8 +25,8 @@ def group_data(data, value_col_names, method="lastValue", group='D'):
         df = df.resample(group).count()
     else:
         assert False
-
     return df
+
 
 def standardize(data1, data2, value_col_name1, value_col_name2):
     """
@@ -41,12 +37,7 @@ def standardize(data1, data2, value_col_name1, value_col_name2):
     :return two DataFrames with conformed datetime points
     """
 
-
-
-
-
-
-    #todo assert dates sorted ascending for both data
+    # todo assert dates sorted ascending for both data
     start_date = max(min(data1['datetime']), min(data2['datetime']))
     end_date = min(max(data1['datetime']), max(data2['datetime']))
 
@@ -61,7 +52,9 @@ def standardize(data1, data2, value_col_name1, value_col_name2):
     if data2['datetime'][end_index_2] == end_date:
         end_index_2 += 1
 
-    data1['datetime'], data1['datetime'] = data1['datetime'][start_index_1:end_index_1], data1['value'][start_index_1:end_index_1]
-    data2['datetime'], data2['datetime'] = data2['datetime'][start_index_2:end_index_2], data2['value'][start_index_2:end_index_2]
+    data1['datetime'], data1['datetime'] = data1['datetime'][start_index_1:end_index_1], data1['value'][
+                                                                                         start_index_1:end_index_1]
+    data2['datetime'], data2['datetime'] = data2['datetime'][start_index_2:end_index_2], data2['value'][
+                                                                                         start_index_2:end_index_2]
 
     return data1, data2
